9784be70ae215fccc0e2dd199ba8eed7177683ad
= <=>
2021-03-20 16:23:28 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemogardenComponent } from './gardening/demogarden/demogarden.component';
+import { GardeningComponent } from './gardening/gardening/gardening.component';
+import { HomeComponent } from './home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'gardening', component: GardeningComponent, children: [
+      { path: 'demogarden', component: DemogardenComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
